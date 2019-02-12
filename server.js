27693dd380628818af786fcbf406c93489ca1621
albertchristianbangan <albertchristianbangan@gmail.com>
2019-02-13 00:52:10 +0800

    modify server

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-    res.render('projects.hbs');
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
 })
 
 app.get('/', (req, res) => {
