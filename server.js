c19581159d00f2e9c507f162514babd435b1663f
albertchristianbangan <albertchristianbangan@gmail.com>
2019-02-12 11:47:21 +0800

    =Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('There is an error');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
+hbs.registerHelper('allCaps', (text) => {
+    return text.toUpperCase();
+})
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Homepage',
+        welcomeNote: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000);
+console.log('App is running on port 3000');
